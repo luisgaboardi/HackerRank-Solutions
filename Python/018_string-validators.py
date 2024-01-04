d23d129e816807083aa5ae786246d7b5051f159d
luisgaboardi <luis.ggaboardi@gmail.com>
2024-01-04 01:54:07 -0300

    Fix wrong exercise number

--- Python/018_string-validators.py
+++ Python/018_string-validators.py
@@ -1,26 +1,8 @@
-n = int(input())
-
-h_block = 'H' * n
-sp_block = ' ' * n
-bd_line = h_block + (sp_block * 3) + h_block
-ct_line = (h_block * 5)
-
-# top triangle
-for ii in range(1, n * 2 + 1, 2):
-    print(("H" * ii).center(2 * n - 1, ' '))
-
-# top body
-for _ in range(n + 1):
-    print(bd_line.center(n * 6 - 1, ' '))
-
-# middle band
-for _ in range(n // 2 + 1):
-    print(ct_line.center(n * 6 - 1, ' '))
-
-# bottom body
-for _ in range(n + 1):
-    print(bd_line.center(n * 6 - 1, ' '))
-
-# bottom triangle
-for ii in range(n * 2 - 1, 0, -2):
-    print(("H" * ii).center(n * 10, ' '))
+if __name__ == '__main__':
+    s = input()
+    
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
